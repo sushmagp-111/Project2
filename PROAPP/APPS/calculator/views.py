7b808304c1d58214f40c1dab23f50b2d6348c6da
sushmagp-111 <78420559+sushmagp-111@users.noreply.github.com>
2021-02-09 12:18:11 +0530

    val3 added

--- PROAPP/APPS/calculator/views.py
+++ PROAPP/APPS/calculator/views.py
@@ -9,6 +9,7 @@ def add(request):
 
     val1 = int(request.GET['num1'])
     val2 = int(request.GET['num2'])
-    res = val1 + val2
+    val3 = int(request.GET['num3'])
+    res = val1 + val2 + val3
 
     return render(request, 'result.html',{'result':res})
